33bd6dfddc75bfccef6f72a2c7c808f652e648ae
BrunoUemura <bruno.h.uemura@outlook.com>
2022-09-17 00:59:08 -0300

    feat: update weather temperature component

--- src/components/Weather/Temperature/index.tsx
+++ src/components/Weather/Temperature/index.tsx
@@ -1,11 +1,42 @@
 import './styles.scss';
 
+import { useState } from 'react';
+
+import cloudy from '../../../assets/conditions/cloudy.png';
+import rainy from '../../../assets/conditions/rainy.png';
+import sunny from '../../../assets/conditions/sunny.png';
 import { ITemperatureProps } from './types';
 
-export default function Temperature({ temperatureInC }: ITemperatureProps) {
+export default function Temperature({
+  condition,
+  temperatureInC,
+  temperatureInF,
+}: ITemperatureProps) {
+  const [selectedScale, setSelectedScale] = useState('C');
+
+  const selectedTemperature = selectedScale === 'C' ? temperatureInC : temperatureInF;
+  const formattedTemperature = `${selectedTemperature}° ${selectedScale}`;
+
+  let weatherCondition;
+
+  if (condition.includes('cloud')) weatherCondition = cloudy;
+  if (condition.includes('rain')) weatherCondition = rainy;
+  if (condition.includes('sun')) weatherCondition = sunny;
+
   return (
     <div className="weather-temperature">
-      <span>{temperatureInC}</span>
+      <div>
+        <img src={weatherCondition} alt={condition} />
+      </div>
+
+      <div>
+        <span className="weather-temperature__current">{formattedTemperature}</span>
+      </div>
+
+      <div className="weather-temperature__scale_options">
+        <button onClick={() => setSelectedScale('C')}>C</button>
+        <button onClick={() => setSelectedScale('F')}>F</button>
+      </div>
     </div>
   );
 }
