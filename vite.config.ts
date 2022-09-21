9e4cf2415e2ba4153a7b1f3ae3c0a62657885fe7
BrunoUemura <bruno.h.uemura@outlook.com>
2022-09-20 23:40:46 -0300

    feat: adds env load in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import 'dotenv/config';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
+
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+
+    server: {
+      port: Number(process.env.VITE_PORT),
+    },
+  });
+};
